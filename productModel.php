d00cb196f861351dc8a372e51a403c0e850a07dd
viviananpin <124177603+viviananpin@users.noreply.github.com>
2023-11-14 14:01:29 +0800

    update

--- productModel.php
+++ productModel.php
@@ -54,6 +54,7 @@ function addCart($jobID,$purchaseAmount) {
     $sql = "update product_list set purchaseAmount=? where id=?"; //SQL中的 ? 代表未來要用變數綁定進去的地方
 	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
 	mysqli_stmt_bind_param($stmt, "ii",$purchaseAmount, $jobID); //bind parameters with variables, with types "sss":string, string ,string
+    mysqli_stmt_execute($stmt);  //執行SQL
     return True;
 }
 function updateJob($id, $jobName,$jobUrgent,$jobContent,$jobDescription) {
