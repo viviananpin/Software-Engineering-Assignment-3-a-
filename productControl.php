3391be6ba86371f11c1529a71c6f18ea9deda1f7
viviananpin <124177603+viviananpin@users.noreply.github.com>
2023-11-14 13:50:15 +0800

    update

--- productControl.php
+++ productControl.php
@@ -7,13 +7,31 @@ case "listJob":
   $jobs=getJobList();
   echo json_encode($jobs);
   return;  
+  
+  
+  case "listPurchaseJob":
+  $jobs=getPurchaseList();
+  echo json_encode($jobs);
+  return;  
+  
+  
 case "addJob":
 	
 	$jsonStr = $_POST['dat'];
 	$job = json_decode($jsonStr);
 	//should verify first
-	addJob($job->jobName,$job->jobUrgent,$job->jobContent,$job->jobDescription);
+	addJob($job->id,$job->jobName,$job->jobUrgent,$job->jobContent,$job->jobDescription);
 	return;
+
+case "addCart":
+	
+	$jsonStr = $_POST['dat'];
+	$job = json_decode($jsonStr);
+	//should verify first
+	addCart($job->id,$job->purchaseAmount);
+	return;
+
+
 case "delJob":
 	$id=(int)$_REQUEST['id']; //$_GET, $_REQUEST
 	//verify
--- productModel.php
+++ productModel.php
@@ -15,16 +15,47 @@ function getJobList() {
 	return $rows;
 }
 
-function addJob($jobName,$jobUrgent,$jobContent,$jobDescription) {
-	global $db;
 
+function getPurchaseList() {
+	global $db;
+	$sql = "select * from product_list WHERE purchaseAmount>0;";
+	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
+	mysqli_stmt_execute($stmt); //執行SQL
+	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
+
+	$rows = array(); //要回傳的陣列
+	while($r = mysqli_fetch_assoc($result)) {
+		$rows[] = $r; //將此筆資料新增到陣列中
+	}
+	return $rows;
+}
+
+
+
+function addJob($jobID,$jobName,$jobUrgent,$jobContent,$jobDescription) {
+	global $db;
+    if($jobID>0){
+    $sql = "update product_list set jobName=?, jobUrgent=?, jobContent=? , jobDescription=? where id=?"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "ssssi", $jobName, $jobUrgent,$jobContent, $jobDescription, $jobID); //bind parameters with variables, with types "sss":string, string ,string
+    }
+
+    else{
 	$sql = "insert into product_list (jobName, jobUrgent, jobContent , jobDescription) values (?,?, ?, ?)"; //SQL中的 ? 代表未來要用變數綁定進去的地方
 	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
 	mysqli_stmt_bind_param($stmt, "ssss", $jobName, $jobUrgent,$jobContent, $jobDescription); //bind parameters with variables, with types "sss":string, string ,string
-	mysqli_stmt_execute($stmt);  //執行SQL
+    }
+    mysqli_stmt_execute($stmt);  //執行SQL
 	return True;
 }
 
+function addCart($jobID,$purchaseAmount) {
+	global $db;
+    $sql = "update product_list set purchaseAmount=? where id=?"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "ii",$purchaseAmount, $jobID); //bind parameters with variables, with types "sss":string, string ,string
+    return True;
+}
 function updateJob($id, $jobName,$jobUrgent,$jobContent,$jobDescription) {
 	echo $id, $jobName,$jobUrgent,$jobContent,$jobDescription;
 	return;
