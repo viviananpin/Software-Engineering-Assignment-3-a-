bb0c54ea8801a718148e61d319c130fe9329e359
viviananpin <124177603+viviananpin@users.noreply.github.com>
2023-12-13 09:54:45 +0800

    update sql

--- order.sql
+++ order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-11-28 14:51:51
+-- 產生時間： 2023-12-13 02:52:00
 -- 伺服器版本： 10.4.28-MariaDB
 -- PHP 版本： 8.2.4
 
@@ -28,12 +28,43 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `order` (
-  `id` int(11) NOT NULL,
+  `orderID` int(11) NOT NULL,
+  `id` varchar(11) NOT NULL,
   `jobName` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `jobUrgent` int(11) DEFAULT NULL,
-  `jobContent` int(11) DEFAULT NULL,
-  `jobDescription` varchar(50) NOT NULL
+  `jobUrgent` varchar(11) DEFAULT NULL,
+  `jobContent` varchar(11) DEFAULT NULL,
+  `jobDescription` varchar(50) NOT NULL,
+  `sellerID` int(11) NOT NULL,
+  `customID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- 傾印資料表的資料 `order`
+--
+
+INSERT INTO `order` (`orderID`, `id`, `jobName`, `jobUrgent`, `jobContent`, `jobDescription`, `sellerID`, `customID`) VALUES
+(14, '3,52', '平板,商品C', '800,300', '1,10', ',Cc', 0, 0),
+(15, '3,4,52', '平板,鍵盤,商品C', '800,400,300', '1,5,10', ',,Cc', 0, 0);
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`orderID`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `order`
+--
+ALTER TABLE `order`
+  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
